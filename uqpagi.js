6d16a441790f72f187795084b8b511fb2f107b37
thwylat710 <84282918+thwylat710@users.noreply.github.com>
2021-09-11 02:25:49 +0200

    Update uqpagi.js

--- uqpagi.js
+++ uqpagi.js
@@ -1,12 +1,3 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE HTML>
-<html xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
-<head>
-  
-  
-  
-  <p style="text-align: center;"><b>&nbsp;جاري تحميل الصفحة</b></p><div class="separator" style="clear: both; text-align: center;"><a href="https://1.bp.blogspot.com/-EAtqEddgMQ0/YTk4IcpKHfI/AAAAAAAAACA/io-7vVR6KVQuZZyyiPy3AV1KJ3gRd1DJQCLcBGAsYHQ/s300/loadbar.gif" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="24" data-original-width="300" height="24" src="https://1.bp.blogspot.com/-EAtqEddgMQ0/YTk4IcpKHfI/AAAAAAAAACA/io-7vVR6KVQuZZyyiPy3AV1KJ3gRd1DJQCLcBGAsYHQ/s0/loadbar.gif" width="300" /></a></div><p style="text-align: center;"><b>Page loading</b></p><p style="text-align: center;"><b><br /></b></p>
-
 
 
 <script type="text/javascript">
@@ -111,30 +102,3 @@ var temp = setInterval("redirect()", 5000);
 </script>
   
   
-  
-    <b:include data='blog' name='all-head-content'/>
-    <title><data:blog.pageTitle/></title>
-
-<b:skin><![CDATA[/*
------------------------------------------------
-Blogger Template Style
-Name:     abduallah mnqush
-Designer: https://abduallah0.blogspot.com
------------------------------------------------ */
-
-
-
-
-
-]]></b:skin>
-
-
-</head>
-<body>
-
-  <b:section id='all'/>
-
-
-
-</body>
-</html>
